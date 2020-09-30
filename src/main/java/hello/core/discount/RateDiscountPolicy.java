1801d331e9307060684aa8553664990575fb4c96
seol <seolnavy@naver.com>
2020-09-30 12:55:52 +0900

    DIP는 준수했지만 NullPointException이 발생한다.

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,21 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+	private int discountPercent = 10;
+
+	@Override
+	public int discount(Member member, int price) {
+		if (!isVIP(member)) {
+			return 0;
+		}
+		return price * discountPercent / 100;
+	}
+
+	private boolean isVIP(Member member) {
+		return member.getGrade() == Grade.VIP;
+	}
+}

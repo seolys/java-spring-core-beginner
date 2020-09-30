ba007c786b638cfb5ef6ade1530cbb2b73720d8d
seol <seolnavy@naver.com>
2020-09-30 13:56:16 +0900

    AppConfig 리팩토링 - Extract Method

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,20 +1,34 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+/**
+ * 역할과 구현 클래스가 한눈에 들어올 수 있도록 한다.
+ * 애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있다.
+ */
 public class AppConfig {
 
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 주입(Constructor Injection).
+		return new MemberServiceImpl(memberRepository()); // 생성자 주입(Constructor Injection).
 	}
 
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
+	}
+
+	private MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
+
+	private DiscountPolicy discountPolicy() {
+		return new FixDiscountPolicy();
 	}
 
 }

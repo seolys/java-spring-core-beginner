c1841f969577b9298943235ee407216cd0c15aec
seol <seolnavy@naver.com>
2020-10-11 14:18:07 +0900

    Request Scope와 ObjectProvider로 Logger구현.

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,35 @@
+package hello.core.common;
+
+import java.util.UUID;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+	private String uuid;
+	private String requestUrl;
+
+	public void setRequestUrl(String requestUrl) {
+		this.requestUrl = requestUrl;
+	}
+
+	public void log(String message) {
+		System.out.println(String.format("[%s][%s] %s", uuid, requestUrl, message));
+	}
+
+	@PostConstruct
+	public void init() {
+		uuid = UUID.randomUUID().toString();
+		System.out.println(String.format("[%s] request scope bean create:" + this, uuid));
+	}
+
+	@PreDestroy
+	public void close() {
+		System.out.println(String.format("[%s] request scope bean close:" + this, uuid));
+	}
+
+}
---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+	private final LogDemoService logDemoService;
+	private final ObjectProvider<MyLogger> myLoggerProvider; // Dependency Lookup
+
+	@RequestMapping("log-demo")
+	@ResponseBody
+	public String logDemo(HttpServletRequest request) {
+		MyLogger myLogger = myLoggerProvider.getObject();
+		String requestUrl = request.getRequestURL().toString();
+		myLogger.setRequestUrl(requestUrl);
+		myLogger.log("controller test");
+		logDemoService.logic("testId");
+		return "OK";
+	}
+
+}

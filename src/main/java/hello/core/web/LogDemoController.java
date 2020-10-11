415462d90f217a8852f8b55390a360cdc97087d8
seol <seolnavy@naver.com>
2020-10-11 14:58:03 +0900

    Request Scope와 Proxy

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,7 +3,6 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final ObjectProvider<MyLogger> myLoggerProvider; // Dependency Lookup
+	//	private final ObjectProvider<MyLogger> myLoggerProvider; // Dependency Lookup
+	private final MyLogger myLogger;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {
-		MyLogger myLogger = myLoggerProvider.getObject();
+//		MyLogger myLogger = myLoggerProvider.getObject();
 		String requestUrl = request.getRequestURL().toString();
 		myLogger.setRequestUrl(requestUrl);
 		myLogger.log("controller test");
